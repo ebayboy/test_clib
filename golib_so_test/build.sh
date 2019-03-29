23b10f04a5e8753bb8b677e70418a771cb59e163
ebayboy <552999964@.com>
2019-03-29 10:22:30 +0000

    fw

--- golib_so_test/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:./
-gcc test_so.c  -I../ -L ../ -ltest_clib -lpthread -o test_so
-./test_so
-
--- golib_so_test/test_so.c
+++
@@ -1,9 +0,0 @@
-
-#include<stdio.h>
-#include"libtest_clib.h" 
-
-void main(){
-    hello();
-    printf("\n2+3=%d\n",add(2,3));
-}
-
